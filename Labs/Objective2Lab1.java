890e5dc37c54874d8abbd8b55cde4029c6ae42ca
Devan Mapp <Devan.R.Mapp@gmail.com>
2022-03-08 14:18:08 -0700

    Complete Objective2Lab1

---
+++ Labs/Objective2Lab1.java
@@ -0,0 +1,7 @@
+public class Objective2Lab1 {
+  public static void main(String[] args) {
+    String hello = "Hello World";
+      System.out.println(hello);
+      //this should print the varible hello, which is stands for Hello World
+  }
+}
