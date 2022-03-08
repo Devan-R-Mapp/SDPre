ccc975a9de4cb9209295e56edbe6b3acb760e1c4
Devan Mapp <Devan.R.Mapp@gmail.com>
2022-03-08 14:37:47 -0700

    Complete Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,11 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Charles Peterson";
+    int age = 47;
+    String state = "Arkansas";
+        System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+        //The nameing prefixes are not considered part of the varibles and should be
+        //part of the println in "" the data can be show by the strings and the int as
+        //shown above.
+  }
+}
